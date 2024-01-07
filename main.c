3b6bef3fbd624b7a90bf33e8ebea139df8c97e34
Alessandro Rosario Torrisi <s330732@studenti.polito.it>
2024-01-07 21:02:20 +0000

    Update main.c

--- main.c
+++ main.c
@@ -27,48 +27,63 @@
 /* FreeRTOS includes. */
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 
 /* Standard includes. */
 #include <stdio.h>
 #include <string.h>
 
+/* creation of binary semaphore global variable */
+SemaphoreHandle_t xMutex;
+
 /* printf() output uses the UART.  These constants define the addresses of the
 required UART registers. */
-#define UART0_ADDRESS 	( 0x40004000UL )
-#define UART0_DATA		( * ( ( ( volatile uint32_t * )( UART0_ADDRESS + 0UL ) ) ) )
-#define UART0_STATE		( * ( ( ( volatile uint32_t * )( UART0_ADDRESS + 4UL ) ) ) )
-#define UART0_CTRL		( * ( ( ( volatile uint32_t * )( UART0_ADDRESS + 8UL ) ) ) )
-#define UART0_BAUDDIV	( * ( ( ( volatile uint32_t * )( UART0_ADDRESS + 16UL ) ) ) )
-#define TX_BUFFER_MASK	( 1UL )
+#define UART0_ADDRESS (0x40004000UL)
+#define UART0_DATA (*(((volatile uint32_t *)(UART0_ADDRESS + 0UL))))
+#define UART0_STATE (*(((volatile uint32_t *)(UART0_ADDRESS + 4UL))))
+#define UART0_CTRL (*(((volatile uint32_t *)(UART0_ADDRESS + 8UL))))
+#define UART0_BAUDDIV (*(((volatile uint32_t *)(UART0_ADDRESS + 16UL))))
+#define TX_BUFFER_MASK (1UL)
 
-void vFullDemoTickHookFunction( void );
+void vFullDemoTickHookFunction(void);
+static void prvPrintTask(void *pvParameters);
+static void prvNewPrintString(const char *pcString);
 
 /*
  * Printf() output is sent to the serial port.  Initialise the serial hardware.
  */
-static void prvUARTInit( void );
+static void prvUARTInit(void);
+void producer(void *pvParameters);
 
 /*-----------------------------------------------------------*/
 
-void main( void )
+void main(void)
 {
-	/* See https://www.freertos.org/freertos-on-qemu-mps2-an385-model.html for
-	instructions. */
+	xMutex = xSemaphoreCreateMutex();
+	/* Check the semaphore was created successfully before creating the tasks. */
+	if (xMutex != NULL)
+	{
+		/* Create two instances of the tasks that write to stdout. The string they
+		write is passed in to the task as the task’s parameter. The tasks are
+		created at different priorities so some pre-emption will occur. */
+		xTaskCreate(prvPrintTask, "Print1", 1000,
+					"Task 1 ***************************************\r\n", 1, NULL);
+		xTaskCreate(prvPrintTask, "Print2", 1000,
+					"Task 2 ---------------------------------------\r\n", 2, NULL);
+		/* Start the scheduler so the created tasks start executing. */
+		vTaskStartScheduler();
+	}
 
-	/* Hardware initialisation.  printf() output uses the UART for IO. */
-	prvUARTInit();
-
-	printf("Prova\r\n");
-
-	xTaskCreate(vTaskFunction, "Task1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-    xTaskCreate(vTaskFunction, "Task2", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-  	xTaskCreate(vTaskFunction, "Task3", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);
-  	vTaskStartScheduler();
-
+	/* If all is well then main() will never reach here as the scheduler will now be
+	running the tasks. If main() does reach here then it is likely that there was
+	insufficient heap memory available for the idle task to be created. Chapter 2
+	provides more information on heap memory management. */
+	for (;;)
+		;
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationMallocFailedHook( void )
+void vApplicationMallocFailedHook(void)
 {
 	/* vApplicationMallocFailedHook() will only be called if
 	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
@@ -83,13 +98,14 @@ void vApplicationMallocFailedHook( void )
 	(although it does not provide information on how the remaining heap might be
 	fragmented).  See http://www.freertos.org/a00111.html for more
 	information. */
-	printf( "\r\n\r\nMalloc failed\r\n" );
+	printf("\r\n\r\nMalloc failed\r\n");
 	portDISABLE_INTERRUPTS();
-	for( ;; );
+	for (;;)
+		;
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationIdleHook( void )
+void vApplicationIdleHook(void)
 {
 	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
 	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
@@ -103,21 +119,22 @@ void vApplicationIdleHook( void )
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName)
 {
-	( void ) pcTaskName;
-	( void ) pxTask;
+	(void)pcTaskName;
+	(void)pxTask;
 
 	/* Run time stack overflow checking is performed if
 	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
 	function is called if a stack overflow is detected. */
-	printf( "\r\n\r\nStack overflow in %s\r\n", pcTaskName );
+	printf("\r\n\r\nStack overflow in %s\r\n", pcTaskName);
 	portDISABLE_INTERRUPTS();
-	for( ;; );
+	for (;;)
+		;
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationTickHook( void )
+void vApplicationTickHook(void)
 {
 	/* This function will be called by each tick interrupt if
 	configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
@@ -127,7 +144,7 @@ void vApplicationTickHook( void )
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationDaemonTaskStartupHook( void )
+void vApplicationDaemonTaskStartupHook(void)
 {
 	/* This function will be called once only, when the daemon task starts to
 	execute (sometimes called the timer task).  This is useful if the
@@ -136,24 +153,24 @@ void vApplicationDaemonTaskStartupHook( void )
 }
 /*-----------------------------------------------------------*/
 
-void vAssertCalled( const char *pcFileName, uint32_t ulLine )
+void vAssertCalled(const char *pcFileName, uint32_t ulLine)
 {
-volatile uint32_t ulSetToNonZeroInDebuggerToContinue = 0;
+	volatile uint32_t ulSetToNonZeroInDebuggerToContinue = 0;
 
 	/* Called if an assertion passed to configASSERT() fails.  See
 	http://www.freertos.org/a00110.html#configASSERT for more information. */
 
-	printf( "ASSERT! Line %d, file %s\r\n", ( int ) ulLine, pcFileName );
+	printf("ASSERT! Line %d, file %s\r\n", (int)ulLine, pcFileName);
 
- 	taskENTER_CRITICAL();
+	taskENTER_CRITICAL();
 	{
 		/* You can step out of this function to debug the assertion by using
 		the debugger to set ulSetToNonZeroInDebuggerToContinue to a non-zero
 		value. */
-		while( ulSetToNonZeroInDebuggerToContinue == 0 )
+		while (ulSetToNonZeroInDebuggerToContinue == 0)
 		{
-			__asm volatile( "NOP" );
-			__asm volatile( "NOP" );
+			__asm volatile("NOP");
+			__asm volatile("NOP");
 		}
 	}
 	taskEXIT_CRITICAL();
@@ -163,13 +180,13 @@ volatile uint32_t ulSetToNonZeroInDebuggerToContinue = 0;
 /* configUSE_STATIC_ALLOCATION is set to 1, so the application must provide an
 implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
 used by the Idle task. */
-void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize)
 {
-/* If the buffers to be provided to the Idle task are declared inside this
-function then they must be declared static - otherwise they will be allocated on
-the stack and so not exists after this function exits. */
-static StaticTask_t xIdleTaskTCB;
-static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
+	/* If the buffers to be provided to the Idle task are declared inside this
+	function then they must be declared static - otherwise they will be allocated on
+	the stack and so not exists after this function exits. */
+	static StaticTask_t xIdleTaskTCB;
+	static StackType_t uxIdleTaskStack[configMINIMAL_STACK_SIZE];
 
 	/* Pass out a pointer to the StaticTask_t structure in which the Idle task's
 	state will be stored. */
@@ -188,13 +205,13 @@ static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
 /* configUSE_STATIC_ALLOCATION and configUSE_TIMERS are both set to 1, so the
 application must provide an implementation of vApplicationGetTimerTaskMemory()
 to provide the memory that is used by the Timer service task. */
-void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize )
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize)
 {
-/* If the buffers to be provided to the Timer task are declared inside this
-function then they must be declared static - otherwise they will be allocated on
-the stack and so not exists after this function exits. */
-static StaticTask_t xTimerTaskTCB;
-static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
+	/* If the buffers to be provided to the Timer task are declared inside this
+	function then they must be declared static - otherwise they will be allocated on
+	the stack and so not exists after this function exits. */
+	static StaticTask_t xTimerTaskTCB;
+	static StackType_t uxTimerTaskStack[configTIMER_TASK_STACK_DEPTH];
 
 	/* Pass out a pointer to the StaticTask_t structure in which the Timer
 	task's state will be stored. */
@@ -210,24 +227,25 @@ static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
 }
 /*-----------------------------------------------------------*/
 
-static void prvUARTInit( void )
+static void prvUARTInit(void)
 {
 	UART0_BAUDDIV = 16;
 	UART0_CTRL = 1;
 }
 /*-----------------------------------------------------------*/
 
-int __write( int iFile, char *pcString, int iStringLength )
+int __write(int iFile, char *pcString, int iStringLength)
 {
 	int iNextChar;
 
 	/* Avoid compiler warnings about unused parameters. */
-	( void ) iFile;
+	(void)iFile;
 
 	/* Output the formatted string to the UART. */
-	for( iNextChar = 0; iNextChar < iStringLength; iNextChar++ )
+	for (iNextChar = 0; iNextChar < iStringLength; iNextChar++)
 	{
-		while( ( UART0_STATE & TX_BUFFER_MASK ) != 0 );
+		while ((UART0_STATE & TX_BUFFER_MASK) != 0)
+			;
 		UART0_DATA = *pcString;
 		pcString++;
 	}
@@ -236,23 +254,34 @@ int __write( int iFile, char *pcString, int iStringLength )
 }
 /*-----------------------------------------------------------*/
 
-void *malloc( size_t size )
+static void prvNewPrintString(const char *pcString)
 {
-	( void ) size;
 
-	/* This project uses heap_4 so doesn't set up a heap for use by the C
-	library - but something is calling the C library malloc().  See
-	https://freertos.org/a00111.html for more information. */
-	printf( "\r\n\r\nUnexpected call to malloc() - should be usine pvPortMalloc()\r\n" );
-	portDISABLE_INTERRUPTS();
-	for( ;; );
-
+	xSemaphoreTake(xMutex, portMAX_DELAY);
+	{
+		printf("%s", pcString);
+		fflush(stdout);
+	}
+	xSemaphoreGive(xMutex);
 }
 
-void vTaskFunction(void *pvParameters) {
-    const char *taskName = pcTaskGetName(NULL);
-    (void)pvParameters; // Ignora l'avviso di parametro non utilizzato
-    printf("La task %s in esecuzione!\r\n", taskName);
-    vTaskDelay(pdMS_TO_TICKS(1000)); // Attendi 1000 millisecondi
-	vTaskDelete(NULL);
+static void prvPrintTask(void *pvParameters)
+{
+	char *pcStringToPrint;
+	const TickType_t xMaxBlockTimeTicks = 0x20;
+	/* Two instances of this task are created. The string printed by the task is
+	passed into the task using the task’s parameter. The parameter is cast to the
+	required type. */
+	pcStringToPrint = (char *)pvParameters;
+	for (;;)
+	{
+		/* Print out the string using the newly defined function. */
+		prvNewPrintString(pcStringToPrint);
+		/* Wait a pseudo random time. Note that rand() is not necessarily reentrant,
+		but in this case it does not really matter as the code does not care what
+		value is returned. In a more secure application a version of rand() that is
+		known to be reentrant should be used - or calls to rand() should be protected
+		using a critical section. */
+		vTaskDelay((rand() % xMaxBlockTimeTicks));
+	}
 }
